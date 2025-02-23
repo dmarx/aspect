b739a804aae0a354d429df36f4838909266dac9b
David Marx <david.marx84@gmail.com>
2025-02-22 23:54:54 -0800

    Update astro.config.mjs

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  base: '/aspect', // replace with repo name
+
   vite: {
     plugins: [tailwindcss()]
   }
