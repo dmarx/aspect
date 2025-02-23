83824b0491ddf74ad1ba7c9164721cadf680f624
David Marx <david.marx84@gmail.com>
2025-02-23 01:10:33 -0800

    Update VaultGraph.tsx

--- web/src/components/VaultGraph.tsx
+++ web/src/components/VaultGraph.tsx
@@ -5,7 +5,6 @@ import { circular } from 'graphology-layout';
 import FA2Layout from 'graphology-layout-forceatlas2';
 import graphData from '../data/vault-graph.json';
 
-
 const VaultGraph = () => {
   const containerRef = useRef(null);
   const sigmaRef = useRef(null);
