d46a3b9d449d67f88bdb8b19e4fed6f2cfdf961a
David Marx <david.marx84@gmail.com>
2025-02-23 13:44:18 -0800

    Update misc.sh

--- ops/misc.sh
+++ ops/misc.sh
@@ -1,1 +1,3 @@
 #!/bin/bash
+
+cd web && npm install @radix-ui/react-slot @radix-ui/react-slider @radix-ui/react-switch @radix-ui/react-dialog lucide-react class-variance-authority clsx tailwind-merge && cd ..
